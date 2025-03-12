defd31382b1e4058d65add39811e26b9cd5d1e9a
Nacim Saafi <nacim.saafi@gmail.com>
2025-03-12 20:50:42 +0100

    Task 4

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,11 @@
-void outer(String name, String id){
-    String inner(){
-        return 'Hello Agent $name your id is $id';
-    }
-    print(inner());
+void outer(String name, String id) {
+  List<String> parts = name.split(" ");
+  String firstName = parts[0];
+  String lastNameInitial = parts[1][0] + ".";
+
+  String inner() {
+    return 'Hello Agent $lastNameInitial$firstName your id is $id';
+  }
+
+  print(inner());
 }
