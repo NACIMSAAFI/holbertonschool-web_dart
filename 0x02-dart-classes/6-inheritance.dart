7d37e009fbca5770b114c0cd686c65e5b5679378
Nacim Saafi <nacim.saafi@gmail.com>
2025-03-20 00:35:56 +0100

    Task 6

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,35 +1,39 @@
 import '6-password.dart';
 
-class User extends Password{
-  int id;
-  String name;
-  int age;
-  double height;
-  String user_password;
+class User extends Password {
+  String? name;
+  int? age;
+  double? height;
+  int? id;
+  String user_password = '';
 
-  User({required this.id, required this.name, required this.age, required this.height, required this.user_password,}) : super(password: user_password);
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'age': age,
-      'height': height,
-      'user_password': user_password,
-    };
+  User(
+      {id = int,
+      name = String,
+      age = int,
+      height = double,
+      user_password = String})
+      : super(password: user_password) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.user_password = user_password;
   }
+
+  Map toJson() => {'id': id, 'name': name, 'age': age, 'height': height};
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
-        id: userJson['id'] ?? 0,
-        name: userJson['name'] ?? 'Unknown',
-        age: userJson['age'] ?? 0,
-        height: userJson['height'] ?? 0.0,
-        user_password: userJson['user_password'] ?? '',
-    );
+    return new User(
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height'],
+        user_password: userJson['user_password']);
   }
 
-  @override
-  String toString() =>
-    "User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height}, Password valid: ${isValid()})";
+  String toString() {
+    final pass = new Password(password: this.user_password);
+    return "User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height}, Password: ${pass.isValid()})";
+  }
 }
